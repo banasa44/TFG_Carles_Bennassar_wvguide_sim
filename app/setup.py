589bbfe1c7588188fd20c9b4ba9eef489d105855
Carles Bennassar <cfbennassar@gmail.com>
2021-04-05 18:23:55 +0200

    ready to work without detectors

--- app/setup.py
+++ app/setup.py
@@ -50,11 +50,13 @@ class Grid ():
 
 class Source ():
     source_xy = [mp.Source(
+        #mp.GaussianSource(constants.Wave.fcen,fwidth=constants.Wave.df),
         mp.ContinuousSource(frequency= constants.Wave.f_max),
         component=mp.Ez,
         center= mp.Vector3(-4.5,0,0))]
     source_xz = [mp.Source(
-        mp.ContinuousSource(frequency= constants.Wave.f_max),
+        mp.GaussianSource(constants.Wave.fcen,fwidth=constants.Wave.df),
+        #mp.ContinuousSource(frequency= constants.Wave.f_max),
         component = mp.Ez,
         center = mp.Vector3(-1.5, -1, 0),
         size = mp.Vector3(6,0,0),
